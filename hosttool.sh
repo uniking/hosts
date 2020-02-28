b69a48f31df47ff30201ac4f3398aa73851c2c1b
uniking <578246786@qq.com>
2020-02-28 13:34:12 +0800

    Create hosttool.sh

---
+++ hosttool.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+scriptPath=$(cd $(dirname $0); pwd)
+
+update()
+{
+    cd ${scriptPath}/bhosts;git pull;
+    cd ${scriptPath}/hosts; git pull;
+    cd ${scriptPath}/AD-hosts; git pull;
+
+    echo "" > ${scriptPath}/merge/hosts
+    cat ${scriptPath}/AD-hosts/system/etc/hosts >> ${scriptPath}/merge/hosts
+    cat ${scriptPath}/hosts/hosts-files/hosts > ${scriptPath}/merge/hosts
+    cat ${scriptPath}/bhosts/hosts >> ${scriptPath}/merge/hosts
+    
+    cd /etc
+    sudo rm hosts
+    sudo ln -s ${scriptPath}/merge/hosts
+}
+
+install()
+{
+    cd ${scriptPath}
+    git clone https://github.com/E7KMbb/AD-hosts.git
+    git clone https://github.com/googlehosts/hosts
+    git clone git@github.com:uniking/bhosts.git
+    mkdir merge
+    update
+}
+
+run()
+{
+    if [ $1 == "install" ]
+        install
+    elif [ $1 == "update" ]
+        update
+    fi
+}
+
+if [ $# -eq 1 ]
+then run $1
+else
+    echo -e "hosttool install\nhosttool update"
+fi
